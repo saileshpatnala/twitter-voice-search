1d3bdb30997b6d3d07d2bfa1cf8b995687d9b9c0
Sailesh Patnala <sailesh.patnala.96@gmail.com>
2022-01-01 18:14:37 -0500

    Initial Commit

---
+++ twitter-voice-search/twitter_voice_searchApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitter_voice_searchApp.swift
+//  twitter-voice-search
+//
+//  Created by Sailesh Patnala on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct twitter_voice_searchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
